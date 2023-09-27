f17dda42d94df8b6d6d64b08b1aeec1cbb0ab66f
ganeshpondy <ganeshpondy@gmail.com>
2023-09-27 11:28:58 +0000

    #29 create cdn and s3 iam policy

--- journal/week1.md
+++ journal/week1.md
@@ -140,3 +140,37 @@ resource "aws_s3_object" "error_html" {
   etag = filemd5(var.error_html_filepath)
 }
 ```
+
+---
+
+## 1.4.0 - CloudFront and S3 Bucket IAM Policy
+
+Create CloudFront and Orgin Access Policy
+
+Create IAM Policy for the S3 Bucket 
+
+``` tf
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  policy = jsonencode({
+    "Version" = "2012-10-17",
+    "Statement" = {
+      "Sid" = "AllowCloudFrontServicePrincipalReadOnly",
+      "Effect" = "Allow",
+      "Principal" = {
+        "Service" = "cloudfront.amazonaws.com"
+      },
+      "Action" = "s3:GetObject",
+      "Resource" = "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*",
+      "Condition" = {
+      "StringEquals" = {
+          "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
+        }
+      }
+    }
+  })
+}
+```
+
+
+
--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -11,41 +11,5 @@ terraform {
 #   # Configuration options
 # }
 
-# Create S3 Bucket
-resource "aws_s3_bucket" "website_bucket" {
-  # Bucket Naming Rules
-  bucket = var.bucket_name
-  tags = {
-    UserUuid = var.user_uuid
-  }
-}
-
-# Enable WebSite in the S3 Bucket
-resource "aws_s3_bucket_website_configuration" "website_configuration" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-# Copy Local Files to S3 Bucket
-resource "aws_s3_object" "index_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "index.html"
-  source = var.index_html_filepath
-
-  etag = filemd5(var.index_html_filepath)   # etag will check the file modified time and copy if the file is modified
-}
-
-resource "aws_s3_object" "error_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "error.html"
-  source = var.error_html_filepath
-
-  etag = filemd5(var.error_html_filepath)
-}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity
+data "aws_caller_identity" "current" {}
