eec402f152e369e8a6992af845e37dcd098de963
ganeshpondy <ganeshpondy@gmail.com>
2023-10-03 01:23:20 +0000

    #39 Terratowns init and plan update

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
-output "bucket_name" {
-  value = module.terrahousr_aws.bucket_name
-}
+# output "bucket_name" {
+#   value = module.terrahousr_aws.bucket_name
+# }
 
 # output "website_endpoint" {
 #   value = module.terrahousr_aws.website_endpoint
 # }
 
-output "cloudfront_url" {
-  value = module.terrahousr_aws.cloudfront_url
-}
+# output "cloudfront_url" {
+#   value = module.terrahousr_aws.cloudfront_url
+# }
 
-output "content_version_value" {
-  value = module.terrahousr_aws.content_version_value
-}
+# output "content_version_value" {
+#   value = module.terrahousr_aws.content_version_value
+# }
